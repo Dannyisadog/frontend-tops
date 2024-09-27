4f2680a88c9dc24e4f4c277423a589ec1f9f75c2
Danny Chen <dannyisadog10@gmail.com>
2024-09-27 23:48:57 +0800

    feat(layout): apply vercel analytics & speed insights component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -21,6 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
