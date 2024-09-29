abb74d2fb322cbc747f39782f1b5940284260134
Danny Chen <dannyisadog10@gmail.com>
2024-09-29 17:15:27 +0800

    feat(metadata): update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,32 @@ export const metadata: Metadata = {
   title: "Frontend Tops",
   description:
     "Frontend Tops is a list of the top frontend repositories on GitHub.",
+  applicationName: "Frontend Tops",
+  authors: [
+    {
+      name: "Danny Chen",
+      url: "https://github.com/Dannyisadog/",
+    },
+  ],
+  generator: "Next.js",
+  keywords: ["frontend", "react", "vue", "angular", "javascript", "typescript"],
+  openGraph: {
+    type: "website",
+    url: "https://frontendtops.dannyisadog.com",
+    title: "Frontend Tops",
+    description:
+      "Frontend Tops is a list of the top frontend repositories on GitHub.",
+    siteName: "Frontend Tops",
+    images: [
+      {
+        url: "https://frontendtops.dannyisadog.com/favicon.ico",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
